88de07755318b337dfa34a6bc5b3985cd4ec0dc9
Umer Farooq <umerfarooq2821035@gmail.com>
2024-12-20 22:26:39 +0500

    Chapter9-11 Completed

---
+++ Assignment03/chapter12-13/app.js
@@ -0,0 +1,26 @@
+// let num1 = +prompt("Enter First Number");
+// let num2 = +prompt("Enter Second Number");
+
+// if(num1 > num2) {
+//     console.log(num1 + " is greater than " + num2);
+// }
+// if(num1 < num2) {
+//     console.log(num1 + " is less than " + num2);
+// }
+// if(num1 == num2) {
+//     console.log(num1 + " is equal to " + num2);
+// }
+
+let num1 = +prompt("Enter a number");
+
+if(num1 < 0) {
+    alert("Your Number is Nagetive");
+}
+
+if(num1 > 0) {
+    alert("Your Number is Positive");
+}
+
+if(num1 == 0) {
+    alert("Your Number is Zero");
+}
---
+++ Assignment03/chapter9-11/app.js
@@ -0,0 +1,183 @@
+// Question 01
+// Answer
+
+// let cityName = prompt("Enter city name");
+
+// if(cityName == "Karachi"){
+//     alert("Karachi is the City Of Lights");
+// }
+
+// Question 02
+// Answer
+
+// let gender = prompt("Enter gender");
+// if(gender == "Male"){ 
+//     alert("Good Morning Sir!");
+// }
+// if(gender == "Female"){
+//     alert("Good Morning Ma'am!");
+// }
+
+// Question 03
+// Answer
+
+// let colorOfSignal = prompt("Enter color of signal");
+
+// if(colorOfSignal == "Red"){
+//     alert("Must Stop!");
+// }
+// if(colorOfSignal == "Yellow"){
+//     alert("Ready To Move!");
+
+// }
+// if(colorOfSignal == "Green"){
+//     alert("Move Now!");
+// }
+
+// Question 04
+// Answer
+
+// let yourFeul = +prompt("Please enter your Fuel");
+
+// if(yourFeul < 0.25){
+//     alert("Please Refuel the Fuel in your Car!");
+// }
+
+// Question 05
+// Answer
+
+// var a = 4;
+// if(++a == 5){
+//     alert("The Given Condition for a is true!");
+// }
+
+// var b = 82;
+// if(b++ == 83){
+//     alert("The Given Condition for b is true!");
+// }
+
+// var c = 12;
+// if(c++ == 13){
+//     alert("Condition 1 is true!");
+// }
+// if(c == 13){
+//     alert("Condition 2 is true!");
+// }
+// if(++c < 14){
+//     alert("Condition 3 is true!");
+// }
+// if(c == 14){
+//     alert("Condition 4 is true!");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCOst = materialCost + laborCost;
+// if(totalCOst == materialCost + laborCost){
+//     alert("The Cost equal");
+// }
+// if(true){
+//     alert("True");
+// }
+
+// if(false){
+//     alert("False");
+// }
+
+// Question 06
+// Answer
+
+// let totalMarks = +prompt("Enter Total marks");
+
+// let obtainedMarks = +prompt("Enter Obtained marks");
+
+// let percentage = (obtainedMarks / totalMarks) * 100;
+
+// let grade;
+// let remarks;
+
+// if(percentage >= 80){
+//     grade = "A-one";
+//     remarks = "Excellent";
+// }
+
+// if(percentage >= 70 && percentage < 80){
+//     grade = "B";
+//     remarks = "Good";
+// }
+
+// if(percentage >= 60 && percentage < 70){
+//     grade = "C";
+//     remarks = "You Need To Improve";
+// }
+// if(percentage < 60){
+//     grade = "fail";
+//     remarks = "sorry";
+// }
+// document.write(`
+//     <h1>Mark Sheet</h1>
+//     <br/>
+//     <h3>Total Marks: ${totalMarks}</h3>
+//     <h3>Obtained Marks: ${obtainedMarks}</h3>
+//     <h3>Percentage: ${percentage}%</h3>
+//     <h3>Grade: ${grade}</h3>
+//     <h3>Remarks: ${remarks}</h3>
+//     `);
+
+
+// Question 07
+// Answer
+
+// let secretNum = 8;
+// let guess = +prompt("Guess a number between 1 and 10");
+// if(guess === secretNum) {
+//     alert("Bingo! Correct Answer!");
+// }
+// if((guess + 1) === secretNum) {
+//     alert("Close enough to correct answer!");
+// }
+
+
+// Question 08
+// Answer
+
+// let num = +prompt("Enter your number...");
+// if(num % 3 === 0) {
+//     alert("The number is divisible by 3!");
+// }
+// if(num % 3 !== 0) {
+//     alert("The number is not divisible by 3!");
+// }
+
+// Question 09
+// Answer
+
+// let num = +prompt("Enter your number...");
+
+// if(num % 2 === 0) {
+//     alert("The number is even!");
+// }
+
+// if(num % 2 !== 0) {
+//     alert("The number is odd!");
+// }
+
+// Question 10
+// Answer
+
+// let temperature = prompt("Enter Temperature");
+
+// if(temperature >= 40){
+//     alert("It's too hot outside!");
+// }
+
+// if(temperature >= 30 && temperature < 40){
+//     alert("The Weather today is normal!");
+// }
+
+// if(temperature >= 20 && temperature < 30){
+//     alert("Today's Weather is Cool!");
+// }
+// if(temperature <10){
+//     alert("OMG! Today's Weather is so Cool");
+// }
