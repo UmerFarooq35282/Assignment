721210b352754cf73ea284ba4024ce2aa7d5daf2
Umer Farooq <umerfarooq2821035@gmail.com>
2024-12-23 19:08:29 +0500

    done with html CSS in Convertor

--- ClassTasks/Task02/index.js
+++ ClassTasks/Task02/index.js
@@ -1,28 +1,36 @@
-let carrency = prompt("Enter Currency Type e.g. ('USD' , 'RIYAL' , 'AUD' , 'EURO')");
-let moneyYouHAve = +prompt("Enter how much currency you have");
+// let carrency = prompt("Enter Currency Type e.g. ('USD' , 'RIYAL' , 'AUD' , 'EURO')");
+// let moneyYouHAve = +prompt("Enter how much currency you have");
 
+
+
+let result = document.getElementById('result');
+let submit = document.getElementById('btn');
 
 let USDRATE = 277.16;
-let RIYALRATE = 74.02;
-let AUDRATE = 177.28;
+let AEDRATE = 75.73;
+let GBPRATE = 354.83;
 let EURORATE = 289.25;
 
 let exchangeRate;
 
-if(carrency.toLowerCase() === "usd"){
-    exchangeRate = USDRATE * moneyYouHAve;
-}
-if(carrency.toLowerCase() === "riyal"){
-    exchangeRate = RIYALRATE * moneyYouHAve;
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    let amount = document.getElementById('amount').value;
+    let carrency = document.getElementById('to').value;
+    if(carrency.toLowerCase() === "usd"){
+        exchangeRate = USDRATE * amount;
+    }
+    if(carrency.toLowerCase() === "aed"){
+        exchangeRate = AEDRATE * amount;
+    
+    }
+    if(carrency.toLowerCase() === "gbp"){
+        exchangeRate = GBPRATE * amount;
+    }
+    if(carrency.toLowerCase() === "eur"){
+        exchangeRate = EURORATE * amount;
+    }
+    result.innerHTML = `<h3>You have total amount ${Math.floor(exchangeRate) || 0}  in PKR </h3>`;
+});
 
-}
-if(carrency.toLowerCase() === "aud"){
-    exchangeRate = AUDRATE * moneyYouHAve;
-}
-
-if(carrency.toLowerCase() === "euro"){
-    exchangeRate = EURORATE * moneyYouHAve;
-}
-
-
-document.write(`<h1>you have total amount ${exchangeRate}  in PKR </h1>`);
+// document.write(`<h1>you have total amount ${exchangeRate}  in PKR </h1>`);
