349c5908f560d9ebcdd6d67ef60d8e20949484d6
Umer Farooq <umerfarooq2821035@gmail.com>
2024-12-29 22:32:05 +0500

    Assignment 04

---
+++ Assignment04/index.js
@@ -0,0 +1,26 @@
+// Cahpter 14 - Arrays
+
+let names = [];
+
+// use JS literal notation for names
+
+names.push("Alice");
+names.push("Bob");
+names.push("Charlie");
+
+// use JS object notation for names
+
+names[names.length] = "David";
+names[names.length] = "Eve";
+names[names.length] = "Frank";
+
+let stringArray = ["Umer" ,"Rehan","Abbas"];
+
+let numbersArray = [1, 2, 3, 4, 5];
+
+let booleanArray = [true, false, true, false];
+
+let mixedArray = ["Alice", 1, true, {name: "Bob"}, [7, 8, 9]];
+
+let qualificationInPakistan = ["SSC" ,"HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M.Phil" , "PhD"];
+
