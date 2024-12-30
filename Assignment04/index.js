502cb0c60f910ece1093fa4436a7a4aab3c3e5e0
Umer Farooq <umerfarooq2821035@gmail.com>
2024-12-30 09:44:29 +0500

    Question 04 Done of Chapter 14

--- Assignment04/index.js
+++ Assignment04/index.js
@@ -24,3 +24,11 @@ let mixedArray = ["Alice", 1, true, {name: "Bob"}, [7, 8, 9]];
 
 let qualificationInPakistan = ["SSC" ,"HSC" , "BCS" , "BS" , "BCOM" , "MS" , "M.Phil" , "PhD"];
 
+let studentsName = ['Umer' , "Farhan" , 'Nadeem'];
+let studentsScore = [450 , 378 , 430];
+
+let totalNumbers = 500;
+
+console.log(`Score of ${studentsName[0]} is ${studentsScore[0]}. Percentage: ${(studentsScore[0]/totalNumbers)*100}%`);
+console.log(`Score of ${studentsName[1]} is ${studentsScore[1]}. Percentage: ${(studentsScore[1]/totalNumbers)*100}%`);
+console.log(`Score of ${studentsName[2]} is ${studentsScore[2]}. Percentage: ${(studentsScore[2]/totalNumbers)*100}%`);
